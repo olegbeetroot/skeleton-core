e55020e540c9775625994876697d81b68bf91ec9
Oleg Draganchuk <oleg.draganchuk@gmail.com>
2021-07-30 13:07:50 +0300

    removed manual classes includes

--- src/SkeletonTheme/Core.php
+++ src/SkeletonTheme/Core.php
@@ -2,13 +2,6 @@
 
 namespace SkeletonTheme;
 
-require('src/BlocksResolver.php');
-require('src/TimberSetup.php');
-require('src/DashboardCustomizer.php');
-require('src/FileStructure.php');
-require('src/EnqueueScripts.php');
-require('src/Manifest.php');
-
 class Core {
     function __construct() {
         $fileStructure = new FileStructure();
